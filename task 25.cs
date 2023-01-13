569d6dad6c6a3aa729b18bb6b43f39a636cd3d98
Nickolay Grigoriev <gnarus@inbox.ru>
2023-01-13 08:27:34 +0300

    first commit tast25

---
+++ task 25.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.Clear();
+int InputNumber(ref int count)
+{
+    count++;
+    while (true)
+    {
+        int num;
+        Console.WriteLine($"Введите число №{count}");
+        bool flag = int.TryParse(Console.ReadLine(), out num);
+        if (flag == true) return num;
+        Console.WriteLine("Ошибка ввода");
+    }
+}
+int MathHw(int A, int B)
+{
+    int rez = Convert.ToInt32(Math.Pow(A, B));
+    return rez;
+}
+int count = 0;
+int num1 = InputNumber(ref count);
+int num2 = InputNumber(ref count);
+int rez = MathHw(num1, num2);
+
+Console.Write(rez);
+
+
+
+
